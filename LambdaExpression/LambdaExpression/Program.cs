c1af6d31f59d67531d0de9130941774c371e8cfb
harshadalohar123 <harshalohar123@gmail.com>
2022-04-10 12:28:31 +0530

    [Harshada]Added:VAlidate User Entry Using Lambda Function

---
+++ LambdaExpression/LambdaExpression/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LambdaExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Lambda problems");
+            LambdaFunction lambda = new LambdaFunction(); // calling
+            lambda.Validate();
+        }
+    }
+}
